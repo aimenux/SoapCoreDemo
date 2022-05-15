d30cfb54861a65c89d24684311255d58dace6770
Aymen TROUDI <Aymen.TROUDI@sodexo.com>
2022-05-15 16:24:47 +0200

    Add tests

--- IntegrationTests/SoapEndpointTests.cs
+++ IntegrationTests/SoapEndpointTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 using FluentAssertions;
 using WebApi;
 
@@ -26,5 +27,67 @@ public class SoapEndpointTests : IClassFixture<WebApiTestFixture>
 
         // assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData("Foo")]
+    [InlineData("Bar")]
+    public async Task ShouldGetValidSoapResponseWithSvcSoapService(string name)
+    {
+        // arrange
+        const string mediaType = "text/xml";
+        const string operationName = "Run";
+        var soapEnvelope = @$"
+          <Envelope xmlns=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:tem=""http://tempuri.org/"" xmlns:web=""http://schemas.datacontract.org/2004/07/WebApi.Models"">
+            <Body>
+                <tem:Run>
+                    <tem:model>
+                        <web:Name>{name}</web:Name>
+                    </tem:model>
+                </tem:Run>
+            </Body>
+         </Envelope>";
+
+        var client = _fixture.CreateClient();
+        var content = new StringContent(soapEnvelope, Encoding.UTF8, mediaType);
+
+        // act
+        var response = await client.PostAsync($"/{Constants.SvcName}?op={operationName}", content);
+        var body = await response.Content.ReadAsStringAsync();
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        body.Should().Contain($"Hello {name}");
+    }
+
+    [Theory]
+    [InlineData("Foo")]
+    [InlineData("Bar")]
+    public async Task ShouldGetValidSoapResponseWithAsmxSoapService(string name)
+    {
+        // arrange
+        const string mediaType = "text/xml";
+        const string operationName = "Run";
+        var soapEnvelope = @$"
+          <Envelope xmlns=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:tem=""http://tempuri.org/"">
+            <Body>
+                <tem:Run>
+                    <tem:model>
+                        <tem:Name>{name}</tem:Name>
+                    </tem:model>
+                </tem:Run>
+            </Body>
+         </Envelope>";
+
+        var client = _fixture.CreateClient();
+        var content = new StringContent(soapEnvelope, Encoding.UTF8, mediaType);
+
+        // act
+        var response = await client.PostAsync($"/{Constants.AsmxName}?op={operationName}", content);
+        var body = await response.Content.ReadAsStringAsync();
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        body.Should().Contain($"Hello {name}");
     }
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Creating soap web service using SoapCore
 ```
 
-> In this repo, i m using [SoapCore](https://github.com/DigDes/SoapCore) in order to create a soap web service in asp net core.
+> In this repo, i m using [SoapCore](https://github.com/DigDes/SoapCore) in order to create a soap web service (asmx, svc) in asp net core.
 >
 
-**`Tools`** : vs22, net 6.0, soap
+**`Tools`** : vs22, net 6.0, soap, xunit, fluent-assertions
